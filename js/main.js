24770025c8c612ed51cd69cc9142b4ac9fef8582
Daniel Caresma Silva <daniel.caresma@gmail.com>
2023-01-26 22:01:29 -0300

    Atualizando as estatísticas do robo

--- js/main.js
+++ js/main.js
@@ -1,8 +1,46 @@
 const controle = document.querySelectorAll("[data-controle]")
+const estatisticas = document.querySelectorAll("[data-estatistica]")
+
+console.log(estatisticas)  // apagar
+
+const pecas = {
+    "bracos": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos":{
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -24
+    },
+    "pernas":{
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 42
+    },
+    "foguetes":{
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
 
 controle.forEach((elemento) => {
     elemento.addEventListener("click", (evento) => {
         manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        atualizaEstatistica(evento.target.dataset.peca)
     })
 })
 
@@ -16,3 +54,6 @@ function manipulaDados (operacao, controle) {
     }
 }
 
+function atualizaEstatistica (peca) {
+    console.log(peca) // apagar
+}
