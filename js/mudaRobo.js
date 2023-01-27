fca92514de19e6c60f14df05f4714364c3a41c29
Daniel Caresma Silva <91802662+DanielCaresma@users.noreply.github.com>
2023-01-27 13:10:02 -0300

    Criando fução mudaRobo

---
+++ js/mudaRobo.js
@@ -0,0 +1,15 @@
+const robo = document.querySelector("#robotron")
+
+robo.addEventListener("click", () => {
+    if(robo.src="img/robotron.png") {
+        robo.src="img/robotron-amarelo.png"
+    }else {
+        robo.src="img/robotron.png"
+    }
+})
+
+
+
+
+
+
