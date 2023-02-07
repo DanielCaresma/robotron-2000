4a81ac6bf24fc8a94b61c4ee566a90c7e8ac0182
Daniel Caresma Silva <daniel.caresma@gmail.com>
2023-02-06 21:22:09 -0300

    Adicionando mudança de cor com um botão

--- js/mudaRobo.js
+++ js/mudaRobo.js
@@ -1,15 +1,25 @@
 const robo = document.querySelector("#robotron")
+const botaoDeCor = document.querySelector("#trocaCor")
 
-robo.addEventListener("click", () => {
-    if(robo.src="img/robotron.png") {
-        robo.src="img/robotron-amarelo.png"
-    }else {
-        robo.src="img/robotron.png"
+const cores = [ 
+    "img/Robotron 2000 - Azul/robotron.png",
+    "img/Robotron 2000 - Branco/robotron.png",
+    "img/Robotron 2000 - Preto/robotron.png",
+    "img/Robotron 2000 - Rosa/robotron.png",
+    "img/Robotron 2000 - Vermelho/robotron.png",
+    "img/Robotron 2000 - Amarelo/robotron.png"
+]
+
+let contador = 0
+
+botaoDeCor.addEventListener("click", () => {
+    if (contador < cores.length) {
+        robo.src = cores[contador]
+        contador++
+    } else {
+        contador = 0
+        robo.src = cores[contador]
+        contador++
     }
 })
-
-
-
-
-
 
