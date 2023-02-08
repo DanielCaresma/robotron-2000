801a1a64fc2ee20f296ed90da02ff9bb12314e36
Daniel Caresma Silva <daniel.caresma@gmail.com>
2023-02-07 23:12:09 -0300

    Criando caixa de seleção e objetos do robo com cores

--- js/mudaRobo.js
+++ js/mudaRobo.js
@@ -1,30 +1,44 @@
 const robo = document.querySelector("#robotron")
-const botaoDeCor = document.querySelector("#trocaCor")
-
-const cores = [ 
-    "img/Robotron 2000 - Azul/robotron.png",
-    "img/Robotron 2000 - Branco/robotron.png",
-    "img/Robotron 2000 - Preto/robotron.png",
-    "img/Robotron 2000 - Rosa/robotron.png",
-    "img/Robotron 2000 - Vermelho/robotron.png",
-    "img/Robotron 2000 - Amarelo/robotron.png"
-]
-
-let contador = 0
-
-botaoDeCor.addEventListener("click", () => {
-    if (contador < cores.length) {
-        robo.src = cores[contador]
-        contador++
-    } else {
-        contador = 0
-        robo.src = cores[contador]
-        contador++
-    }
-})
+const caixa = document.querySelector("#caixa-de-selecao")
+const robos = {
+    azul: "img/Robotron 2000 - Azul/robotron.png",
+    branco: "img/Robotron 2000 - Branco/robotron.png",
+    preto: "img/Robotron 2000 - Preto/robotron.png",
+    rosa: "img/Robotron 2000 - Rosa/robotron.png",
+    vermelho: "img/Robotron 2000 - Vermelho/robotron.png",
+    amarelo: "img/Robotron 2000 - Amarelo/robotron.png"
+}
 
 
-// Função para alterar as cores com botões de seleção
+
+// =====> Muda o robo percorrendo o array com os endereços de imagem <========
+
+// const robo = document.querySelector("#robotron")
+// const botaoDeCor = document.querySelector("#trocaCor")
+
+// const cores = [
+//     "img/Robotron 2000 - Azul/robotron.png",
+//     "img/Robotron 2000 - Branco/robotron.png",
+//     "img/Robotron 2000 - Preto/robotron.png",
+//     "img/Robotron 2000 - Rosa/robotron.png",
+//     "img/Robotron 2000 - Vermelho/robotron.png",
+//     "img/Robotron 2000 - Amarelo/robotron.png"
+// ]
+
+// let contador = 0
+
+// botaoDeCor.addEventListener("click", () => {
+//     if (contador < cores.length) {
+//         robo.src = cores[contador]
+//         contador++
+//     } else {
+//         contador = 0
+//         robo.src = cores[contador]
+//         contador++
+//     }
+// })
+
+// ==========> Função para alterar as cores com botões de seleção <================
 
 // function trocaImagem(cor){
 //     document.querySelector(".robo").src="./img/Robotron 2000 - " + cor + "/robotron.png";
